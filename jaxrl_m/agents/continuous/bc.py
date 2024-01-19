6aa8b7b88cab048fe492d163c57affa37bf2dbc1
lihenglin <lhlin@stanford.edu>
2024-01-19 10:05:33 -0800

    fix bc training logging

--- jaxrl_m/agents/continuous/bc.py
+++ jaxrl_m/agents/continuous/bc.py
@@ -43,8 +43,8 @@ class BCAgent(flax.struct.PyTreeNode):
                 {
                     "actor_loss": actor_loss,
                     "mse": mse.mean(),
-                    "log_probs": log_probs,
-                    "pi_actions": pi_actions,
+                    "log_probs": log_probs.mean(),
+                    "pi_actions": pi_actions.mean(),
                     "mean_std": actor_std.mean(),
                     "max_std": actor_std.max(),
                 },
