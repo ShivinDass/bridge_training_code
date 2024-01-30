2bd5c25fb656270896208b0eb17f3f5ff419fb17
lihenglin <lhlin@stanford.edu>
2024-01-30 10:19:32 -0800

    update retrieval scripts

--- experiments/retrieve_precomputed.py
+++ experiments/retrieve_precomputed.py
@@ -17,11 +17,11 @@ from jaxrl_m.data.text_processing import text_processors
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string("checkpoint_path", None, "Path to the checkpoint to load.", required=True)
-flags.DEFINE_string("target_dataset_path", None, "Path to the target dataset.", required=True)
 flags.DEFINE_string("prior_dataset_path", None, "Path to the prior dataset.", required=True)
+flags.DEFINE_string("precomputed_sim_scores_path", None, "Path to precomputed sim scores", required=True)
 flags.DEFINE_float("threshold", 0.1, "Threshold for retrieval.")
 flags.DEFINE_string("output_dir", None, "Path to the output directory.", required=True)
+flags.DEFINE_string("postfix", "", "Postfix for the output path.")
 
 config_flags.DEFINE_config_file(
     "config",
@@ -42,79 +42,22 @@ def main(_):
     tf.config.set_visible_devices([], "GPU")
 
     # load datasets
-    target_paths = [glob_to_path_list(FLAGS.target_dataset_path, prefix=FLAGS.config.data_path)]
     prior_paths  = [glob_to_path_list(FLAGS.prior_dataset_path,  prefix=FLAGS.config.data_path)]
-
-    target_paths = [[os.path.join(path, "train/out.tfrecord") for path in sub_list] for sub_list in target_paths]
-    prior_paths  = [[os.path.join(path, "train/out.tfrecord") for path in sub_list] for sub_list in prior_paths]
-
-    target_data = BridgeRetrievalDataset(
-        target_paths,
-        FLAGS.config.seed,
-        batch_size=FLAGS.config.batch_size,
-    )
+    prior_paths  = [sorted([os.path.join(path, "train/out.tfrecord") for path in sub_list]) for sub_list in prior_paths]
     prior_data = BridgeRetrievalDataset(
         prior_paths,
         FLAGS.config.seed,
         batch_size=FLAGS.config.batch_size,
     )
 
-    target_data_iter = target_data.tf_dataset.as_numpy_iterator()
-    prior_data_iter  = prior_data.tf_dataset.as_numpy_iterator()
-    
-    target_batch = next(target_data_iter)
-    prior_batch  = next(prior_data_iter)
-
-    # define encoder
-    encoder_def = encoders[FLAGS.config.encoder](**FLAGS.config.encoder_kwargs)
-
-    # define decoder
-    decoder_def = decoders[FLAGS.config.decoder](**FLAGS.config.decoder_kwargs)
-
-    # initialize agent
-    rng = jax.random.PRNGKey(FLAGS.config.seed)
-    rng, construct_rng = jax.random.split(rng)
-    agent = pretrain_agents[FLAGS.config.agent].create(
-        rng=construct_rng,
-        observations=target_batch,
-        encoder=encoder_def,
-        decoder=decoder_def,
-        **FLAGS.config.agent_kwargs,
-    )
-    agent = checkpoints.restore_checkpoint(FLAGS.checkpoint_path, target=agent)
-
-    target_embeddings = []
-    while True:
-        target_embeddings.append(agent.compute_embeddings(target_batch))
-
-        try:
-            target_batch = next(target_data_iter)
-        except StopIteration:
-            break
-    target_embeddings = jnp.concatenate(target_embeddings, axis=0)
-    logging.info(f"target size: {target_embeddings.shape[0]}")
-    logging.info("Finish computing target embeddings.")
-
-    sim_scores = []
-    while True:
-        prior_embeddings = agent.compute_embeddings(prior_batch)
-        sim_scores.append(-jnp.min(scipy.spatial.distance.cdist(target_embeddings, prior_embeddings), axis=0))
-
-        try:
-            prior_batch = next(prior_data_iter)
-        except StopIteration:
-            break
-    sim_scores = jnp.concatenate(sim_scores, axis=0)
-    logging.info(f"prior size: {sim_scores.shape[0]}")
-    logging.info("Finish computing similarity scores.")
-
+    sim_scores = np.load(FLAGS.precomputed_sim_scores_path)
     retrieval_distances = -sim_scores
     sorted_distances = np.argsort(retrieval_distances)
     threshold_idx = sorted_distances[:int(FLAGS.threshold * len(sorted_distances))]
     mask = np.zeros_like(retrieval_distances, dtype=np.bool_)
     mask[threshold_idx] = True
 
-    outpath = os.path.join(FLAGS.output_dir, f"{FLAGS.prior_dataset_path.split('/')[0]}_{FLAGS.threshold}", 'train/out.tfrecord')
+    outpath = os.path.join(FLAGS.output_dir, f"{FLAGS.prior_dataset_path.split('/')[0]}_{FLAGS.postfix}_{FLAGS.threshold}", 'train/out.tfrecord')
     tf.io.gfile.makedirs(os.path.dirname(outpath))
     with tf.io.TFRecordWriter(outpath) as writer:
         prior_data_iter  = prior_data.tf_dataset.as_numpy_iterator()
@@ -123,6 +66,8 @@ def main(_):
         while True:
             try:
                 prior_batch = next(prior_data_iter)
+                if logger_step == 0:
+                    logging.info(f"First three actions of the first batch: {prior_batch['actions'][:3]}")
                 current_mask = mask[current_idx:current_idx+len(prior_batch['terminals'])]
                 current_idx += len(prior_batch['terminals'])
                 logger_step += 1
